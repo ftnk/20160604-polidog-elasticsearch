958336f92397e5977788322a1fa9c397f31dff76
Fumihisa TONAKA <fumi.ftnk@gmail.com>
2016-06-02 14:29:38 +0900

    bin/setup.sh: install dstat

--- bin/setup.sh
+++ bin/setup.sh
@@ -6,12 +6,14 @@ rpm -q puppetlabs-release > /dev/null || \
 rpm -q puppet > /dev/null || \
     yum install -y puppet
 
+# install dstat
+yum install -y http://pkgs.repoforge.org/dstat/dstat-0.7.2-1.el6.rfx.noarch.rpm
+
 # Run Puppet
 puppet apply \
        --modulepath=$(pwd)/puppet/modules \
        --verbose \
        puppet/manifests/site.pp
-
 
 [ ! -f /usr/local/bin/embulk ] && \
     curl -o /usr/local/bin/embulk -L "http://dl.embulk.org/embulk-latest.jar" && \
