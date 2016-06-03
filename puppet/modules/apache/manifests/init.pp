21afee1ef7b26fa2e65ba52f365eda311a1b3614
Fumihisa TONAKA <fumi.ftnk@gmail.com>
2016-06-04 08:32:50 +0900

    puppet/modules/apache/manifests/init.pp: ログをとるための設定いろいろ

--- puppet/modules/apache/manifests/init.pp
+++ puppet/modules/apache/manifests/init.pp
@@ -6,9 +6,10 @@ class apache (){
   }
 
   service { 'apache':
-    ensure => running,
-    enable => true,
-    name   => 'httpd',
+    ensure  => running,
+    enable  => true,
+    name    => 'httpd',
+    require => Package['apache'],
   }
 
   firewall { '100 allow inbound http (80)':
@@ -29,5 +30,44 @@ class apache (){
     owner  => 'root',
     group  => 'td-agent',
     mode   => '750',
+    require => Package['td-agent'],
+  }
+
+  file { '/var/www/combined':
+    ensure  => directory,
+    require => Package['apache'],
+  }
+
+  file { '/var/www/combined/logs':
+    ensure => directory,
+    mode   => '755',
+  }
+
+  file { '/var/www/combined/htdocs':
+    ensure => directory,
+  }
+
+  file { '/var/www/combined/htdocs/index.html':
+    ensure  => file,
+    content => "combined\n",
+  }
+
+  file { '/var/www/ltsv':
+    ensure  => directory,
+    require => Package['apache'],
+  }
+
+  file { '/var/www/ltsv/logs':
+    ensure => directory,
+    mode   => '755',
+  }
+
+  file { '/var/www/ltsv/htdocs':
+    ensure => directory,
+  }
+
+  file { '/var/www/ltsv/htdocs/index.html':
+    ensure  => file,
+    content => "ltsv\n",
   }
 }
